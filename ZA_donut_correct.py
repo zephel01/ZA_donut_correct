b7e39df6fc75e0a15de16809bd294c7ae89c670e
Hideki Yamamoto <zephel01@gmail.com>
2026-02-10 05:50:07 +0900

    ドキュメント追加

--- ZA_donut_correct.py
+++ ZA_donut_correct.py
@@ -1078,7 +1078,8 @@ class ZA_DonutV188(ImageProcPythonCommand):
         ポケモンセンター（ベール）へ移動してセーブするメソッド
         """
         self.log("ポケモンセンター（ベール）へ移動してセーブします")
-
+        # キャンセルでフィールドに戻る
+        self.pressRep(Button.B, repeat=5, interval=0.5)
         # メニューを開く
         self.press(Button.PLUS, 0.2, 1.0)
         # 地図を開く
